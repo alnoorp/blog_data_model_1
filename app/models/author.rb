3be40ccf53c9736ce481c82e8068cc6e654925f2
Alnoor Pirani <alnoorp@gmail.com>
2013-12-12 15:35:36 -0500

    define author table model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
+message: "Not a valid email address" }
 end
