b011a922ebb6bfff83d8ee99f42eea9a855158f0
Alnoor Pirani <alnoorp@gmail.com>
2013-12-29 19:15:55 -0500

    add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230000937) do
+ActiveRecord::Schema.define(version: 20131230001427) do
 
   create_table "authors", force: true do |t|
     t.string   "name",       null: false
     t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.datetime "date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
