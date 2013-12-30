46ddea38e1d530ab090f72d3184aa8cf25d59875
Alnoor Pirani <alnoorp@gmail.com>
2013-12-29 19:18:47 -0500

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131230001427) do
+ActiveRecord::Schema.define(version: 20131230001605) do
 
   create_table "authors", force: true do |t|
     t.string   "name",       null: false
@@ -20,9 +20,15 @@ ActiveRecord::Schema.define(version: 20131230001427) do
     t.datetime "updated_at"
   end
 
+  create_table "categories", force: true do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", force: true do |t|
-    t.text     "content"
-    t.datetime "date"
+    t.text     "content",    null: false
+    t.datetime "date",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
